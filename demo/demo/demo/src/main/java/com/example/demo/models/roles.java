72eebbab6ffc7a3b942d1a5c84661f8ec7577a61
Roshini2301 <153311555+Roshini2301@users.noreply.github.com>
2025-07-09 15:52:10 +0530

    day 6 afternoon

---
+++ demo/demo/demo/src/main/java/com/example/demo/models/roles.java
@@ -0,0 +1,21 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int roleId;
+    private String roleName;
+
+}
--- demo/demo/demo/src/main/java/com/example/demo/services/AuthService.java
+++ demo/demo/demo/src/main/java/com/example/demo/services/AuthService.java
@@ -3,6 +3,7 @@ package com.example.demo.services;
 import com.example.demo.models.RegisterDetails;
 import com.example.demo.repository.RegisterDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -11,13 +12,16 @@ import java.util.Objects;
 public class AuthService {
     @Autowired
     RegisterDetailsRepository registerDetailsRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     public String addNewEmployee(RegisterDetails register) {
         RegisterDetails registerDetails = new RegisterDetails();
         registerDetails.setEmpId(register.getEmpId());
         registerDetails.setEmail(register.getEmail());
         registerDetails.setGender(register.getGender());
         registerDetails.setRole(register.getRole());
-        registerDetails.setPassword(register.getPassword());
+        System.out.println("password is"+register.getPassword()+"\nEncrypted password is"+passwordEncoder.encode(register.getPassword()));
+        registerDetails.setPassword(passwordEncoder.encode(register.getPassword()));
         registerDetails.setDateOfBirth(register.getDateOfBirth());
         registerDetails.setEmpname(registerDetails.getEmpname());
         registerDetailsRepository.save(registerDetails);
@@ -27,7 +31,7 @@ public class AuthService {
                 public String authenticate (RegisterDetails login){
                     RegisterDetails user = registerDetailsRepository.findByEmail(login.getEmail());
                     if (user != null) {
-                        if (Objects.equals(user.getPassword(), login.getPassword())) {
+                        if (passwordEncoder.matches(login.getPassword(), user.getPassword())) {
                             return "Login Successful";
                         } else {
                             return "Login not successfull";
